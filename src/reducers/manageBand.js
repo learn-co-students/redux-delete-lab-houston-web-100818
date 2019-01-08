131c47daf6b3d383cb17fa95364e82dee7d399f9
barbaradecares <barbaradecares@hotmail.com>
2019-01-08 14:12:04 -0600

    done

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,12 +1,18 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
+export default function manageBand(
+  state = {
+    bands: []
+  },
+  action
+) {
   switch (action.type) {
-    case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
+    case "ADD_BAND":
+      return { ...state, bands: [...state.bands, action.band] };
+      break;
+    case "DELETE_BAND":
+      return { bands: state.bands.filter(band => band.id !== action.bandId) };
+      break;
 
     default:
       return state;
   }
-};
+}
